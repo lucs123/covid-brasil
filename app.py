9fc3b118539a1a5662f42d3c06cb34b43e32dbb3
Lucas <lucasf854@gmail.com>
2020-04-14 21:44:12 -0300

    name added to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.LUX])
 app.title = 'Covid Brasil'
 
 app.config.suppress_callback_exceptions = True
